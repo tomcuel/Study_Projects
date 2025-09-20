ba0d8a309912f540f861c6fd68b98d46b7441e51
Tom Cuel <tom.cuel3@gmail.com>
2025-09-20 10:11:16 +0200

    Functions to handle sounds for the gameplay

---
+++ Chess_Game_C_IN104/Sounds/Audio.c
@@ -0,0 +1,35 @@
+#include "Audio.h"
+    
+    
+void play_sound(const char* filename) {
+	// local variables
+	static Uint32 wav_length; // length of our sample
+	static Uint8 *wav_buffer; // buffer containing our audio file
+	static SDL_AudioSpec wav_spec; // the specs of our piece of music
+	
+	// Load the WAV
+	// the specs, length and buffer of our wav are filled
+    // check if the file is loaded
+	if( SDL_LoadWAV(filename, &wav_spec, &wav_buffer, &wav_length) == NULL ){
+        printf("Failed to load wav file: %s\n", SDL_GetError());
+	    SDL_FreeWAV(wav_buffer);
+	}
+	
+    // open audio device
+    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wav_spec, NULL, 0);
+    // play audio
+    int success = SDL_QueueAudio(deviceId, wav_buffer, wav_length);
+    if (success != 0) {
+        fprintf(stderr, "Failed to queue audio: %s\n", SDL_GetError());
+        SDL_FreeWAV(wav_buffer);
+    }
+    SDL_PauseAudioDevice(deviceId, 0);
+    // keep application running long enough to hear the sound
+    
+    // wait for sound to finish
+    SDL_Delay(300);
+
+    // clean up
+    SDL_CloseAudioDevice(deviceId);
+	SDL_FreeWAV(wav_buffer);
+}
